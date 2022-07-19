2822a1f276ba364c61ad94754c91c0cc018d297d
yeonlisa <tkzw4869@gmail.com>
2022-07-19 16:22:26 +0900

    Paths

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(0, 0, 800, 800);
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(350, 350, 100, 100);
+ctx.rect(450, 450, 100, 100);
+ctx.fillStyle = "red";
+ctx.fill();
