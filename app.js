1f9b2d82821cd1360554f1fce6cb6d74a82ea572
yeonlisa <tkzw4869@gmail.com>
2022-07-22 11:57:19 +0900

    Paint Color part One

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -28,9 +29,15 @@ function onLineWidthChange(event) {
     ctx.lineWidth = event.target.value;
 }
 
+function onColorChange(event) {
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
